23a238b4aa00371332464e4d89c61096a61e9b30
evanh237 <evanh237@gmail.com>
2024-03-17 17:47:23 -0500

    added increment/decrement/custom function

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,60 @@
 import CartItemCard from "./CartItemCard";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Cart = ({ cart, products, setCart }) => {
+  const [cartTotal, setCartTotal] = useState(0);
+
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
 
+  useEffect(() => {
+    const total = cart.reduce(
+      (acc, item) => {
+        const productItem = getCartItemDetails(item);
+        return acc + (productItem ? productItem.price * item.quantity : 0);
+      },
+
+      0
+    );
+
+    setCartTotal(total);
+  }, [cart, products]);
+
   const getCartItemDetails = (cartItem) =>
     products.find((product) => product.id === cartItem.productId);
+
+  const handleIncrement = (productId, quantityToAdd) => {
+    const updatedCart = cart.map((item) => {
+      if (item.productId === productId) {
+        return {
+          ...item,
+          quantity: item.quantity + quantityToAdd,
+        };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  };
+
+  const handleDecrement = (productId, quantityToSubtract) => {
+    const updatedCart = cart.map((item) => {
+      if (item.productId === productId) {
+        const newQuantity = item.quantity - quantityToSubtract;
+        const updatedQuantity = Math.max(newQuantity, 0);
+        return {
+          ...item,
+          quantity: updatedQuantity,
+        };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  };
+
+  const handleClearCart = () => {
+    setCart([]);
+  };
 
   return (
     <div>
@@ -20,9 +67,13 @@ const Cart = ({ cart, products, setCart }) => {
             key={productItem?.id}
             cartItem={productItem}
             quantity={item.quantity}
+            onIncrement={handleIncrement}
+            onDecrement={handleDecrement}
           />
         );
       })}
+      <div>Cart Total: ${cartTotal}</div>
+      <button onClick={handleClearCart}>Clear Cart</button>
     </div>
   );
 };
--- src/components/CartItemCard.jsx
+++ src/components/CartItemCard.jsx
@@ -1,6 +1,24 @@
+import { useState } from "react";
 import "./CartItemCard.css";
-const CartItemCard = ({ cartItem, quantity }) => {
-  console.log("cart item-->", cartItem);
+const CartItemCard = ({ cartItem, quantity, onIncrement, onDecrement }) => {
+  const [customQuantity, setCustomQauntity] = useState(1);
+
+  const handleCustomQuantityChange = (e) => {
+    const value = parseInt(e.target.value);
+    setCustomQauntity(value);
+  };
+
+  const handleIncrement = () => {
+    onIncrement(cartItem.id, 1);
+  };
+
+  const handleDecrement = () => {
+    onDecrement(cartItem.id, 1);
+  };
+
+  const handleAddToCart = () => {
+    onIncrement(cartItem.id, customQuantity);
+  };
 
   return (
     <div className="cart-item-card">
@@ -16,8 +34,24 @@ const CartItemCard = ({ cartItem, quantity }) => {
         <p>Price: ${cartItem?.price}</p>
         <p>Quantity: {quantity}</p>
       </div>
+      <div className="quantity-buttons">
+        <button onClick={handleIncrement}>+</button>
+        <button onClick={handleDecrement}>-</button>
+      </div>
+      <div className="quantity-custom-button">
+        <input
+          type="numer"
+          value={customQuantity}
+          min="1"
+          onChange={handleCustomQuantityChange}
+        />
+        <button onClick={handleAddToCart}>Add</button>
+      </div>
     </div>
   );
 };
 
 export default CartItemCard;
+
+//add logic to remove item from cart if quantity = 0,
+//this is for single item cards, but add some kind of feedback that the item is added
